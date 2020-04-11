5122727f7e4e4c40040466de19d8f5be7c73cbe5
denisbaranov <dbaranov.work@gmail.com>
2020-04-12 01:28:37 +0300

    Done: Save restriction data for the menu item on the page [Appearance > Customize > Menus ]

--- includes/admin/assets/js/um-admin-nav-menu.js
+++ includes/admin/assets/js/um-admin-nav-menu.js
@@ -1,43 +1,87 @@
 jQuery(document).ready(function ($) {
-	var template = wp.template( 'um-nav-menus-fields' );
 
-	$( document ).on( 'menu-item-added', function ( e, $menuMarkup ) {
-		var id = $( $menuMarkup ).attr('id').substr(10);
+	/**
+	 * This code is not used if $wp_version >= '5.4'
+	 */
+	if ( typeof (um_menu_restriction_data.menu_data) === 'object' ) {
+		var template = wp.template('um-nav-menus-fields');
 
-		var template_content = template({
-			menu_item_id: id,
-			restriction_data:{
-				um_nav_columns:2,
-				um_nav_public:0,
-				um_nav_roles:[],
-				um_nav_roles_all:um_menu_restriction_data.roles_all
+		/**
+		 * Add block "Ultimate Member Menu Settings" to a new menu item
+		 */
+		$(document).on('menu-item-added', function (e, $menuMarkup) {
+			var id = $($menuMarkup).attr('id').substr(10);
+
+			var template_content = template({
+				menu_item_id: id,
+				restriction_data: {
+					um_nav_columns: 2,
+					um_nav_public: 0,
+					um_nav_roles: [],
+					um_nav_roles_all: um_menu_restriction_data.roles_all
+				}
+			});
+
+			if ( $($menuMarkup).find('fieldset.field-move').length > 0 ) {
+				$($menuMarkup).find('fieldset.field-move').before(template_content);
+			} else {
+				$($menuMarkup).find('.menu-item-actions').before(template_content);
 			}
 		});
 
-		if ( $( $menuMarkup ).find( 'fieldset.field-move' ).length > 0 ) {
-			$( $menuMarkup ).find( 'fieldset.field-move' ).before( template_content );
-		} else {
-			$( $menuMarkup ).find( '.menu-item-actions' ).before( template_content );
-		}
-	});
 
-
-	/**
-	 * The variable um_menu_restriction_data.menu_data appears if $wp_version < '5.4'
-	 */
-	if( typeof( um_menu_restriction_data.menu_data ) === 'object' ){
-		$( 'ul#menu-to-edit > li' ).each( function(){
+		/**
+		 * Add block "Ultimate Member Menu Settings" to existed menu items
+		 */
+		$('ul#menu-to-edit > li').each(function () {
 			var id = $(this).attr('id').substr(10);
 			var template_content = template({
 				menu_item_id: id,
 				restriction_data: um_menu_restriction_data.menu_data[ id ]
 			});
 
-			if ( $( this ).find( 'fieldset.field-move' ).length > 0 ) {
-				$( this ).find( 'fieldset.field-move' ).before( template_content );
+			if ( $(this).find('fieldset.field-move').length > 0 ) {
+				$(this).find('fieldset.field-move').before(template_content);
 			} else {
-				$( this ).find( '.menu-item-actions' ).before( template_content );
+				$(this).find('.menu-item-actions').before(template_content);
 			}
 		});
 	}
+
+
+	/**
+	 * Update restriction data for the menu item on the page [Appearence > Customize > Menus]
+	 * @since 2.1.6
+	 */
+	jQuery('#customize-theme-controls').on('change', 'div.um-nav-edit', function (e) {
+		var $parent = jQuery(e.currentTarget);
+		var $target = jQuery(e.target);
+
+		/* Toggle roles block */
+		if ( $target.is('select[name*=um_nav_public]') ) {
+			if ( $target.val() == 2 ) {
+				$parent.find('p.um-nav-roles').show();
+			} else {
+				$parent.find('p.um-nav-roles').hide();
+			}
+		}
+
+		/* Prepare request */
+		var data = $parent.find("input, textarea, select").serializeArray();
+		data.push({
+			name: 'action',
+			value: 'um-customize-update_nav_menu_item'
+		});
+		data.push({
+			name: 'menu_item_id',
+			value: $parent.data('menu_item_id')
+		});
+
+		/* Process AJAX request to update menu item restriction */
+		wp.ajax.post('um-customize-update_nav_menu_item', data).fail(function (response) {
+			if ( typeof (response) === 'string' ) {
+				console.log(response);
+			}
+		});
+	});
 });
--- includes/admin/core/class-admin-navmenu.php
+++ includes/admin/core/class-admin-navmenu.php
@@ -18,7 +18,17 @@ if ( !class_exists( 'um\admin\core\Admin_Navmenu' ) ) {
 		 * @var array
 		 */
 		protected static $fields = array();
+
+		/**
+		 * The restriction data for menu items
+		 * @var array
+		 */
 		protected static $restriction_data = array();
+
+		/**
+		 * The list of existing roles
+		 * @var array
+		 */
 		protected static $roles = array();
 
 		function __construct() {
@@ -40,8 +50,10 @@ if ( !class_exists( 'um\admin\core\Admin_Navmenu' ) ) {
 			 * @link https://make.wordpress.org/core/2020/02/25/wordpress-5-4-introduces-new-hooks-to-add-custom-fields-to-menu-items/
 			 */
 			if ( $wp_version >= '5.4' ) {
+				add_action( 'customize_controls_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
 				add_action( 'customize_register', array( $this, 'customize_register' ), 90 );
 				add_action( 'customize_save', array( $this, 'customize_save' ), 90 );
+				add_action( 'wp_ajax_um-customize-update_nav_menu_item', array( $this, 'customize_update_nav_menu_item' ) );
 				add_action( 'wp_nav_menu_item_custom_fields', array( $this, 'wp_nav_menu_item_custom_fields' ), 20, 5 );
 				add_action( 'wp_nav_menu_item_custom_fields_customize_template', array( $this, 'wp_nav_menu_item_custom_fields_customize_template' ), 20 );
 			}
@@ -99,7 +111,7 @@ if ( !class_exists( 'um\admin\core\Admin_Navmenu' ) ) {
 		}
 
 		/**
-		 *
+		 * The template for a new menu items.
 		 */
 		function _wp_template() {
 			?>
@@ -118,7 +130,8 @@ if ( !class_exists( 'um\admin\core\Admin_Navmenu' ) ) {
 			UM()->admin_enqueue()->load_nav_manus_scripts();
 
 			$menu_restriction_data = array(
-				'roles_all'  => UM()->roles()->get_roles( false, array( 'administrator' ) )
+				'roles_all'  => UM()->roles()->get_roles( false, array( 'administrator' ) ),
+				'wp_version' => $wp_version
 			);
 
 			if ( $wp_version < '5.4' ) {
@@ -153,35 +166,34 @@ if ( !class_exists( 'um\admin\core\Admin_Navmenu' ) ) {
 		}
 
 		/**
-		 * Save restriction data for the menu item
-		 *
-		 * @todo   SAVE CHANGED SETTINGS
+		 * AJAX handler: Save restriction data for the menu item on the page [Appearence > Customize > Menus ]
 		 *
 		 * @since  2.1.6
-		 * @hook   customize_save
-		 *
-		 * @param  WP_Customize_Manager  $wp_customize
+		 * @hook   wp_ajax_um-customize-update_nav_menu_item
 		 */
-		public function customize_save( $wp_customize ) {
-			$controls = $wp_customize->controls();
+		public function customize_update_nav_menu_item() {
+
+			$menu_item_id = filter_input( INPUT_POST, 'menu_item_id', FILTER_SANITIZE_NUMBER_INT );
+			if ( empty( $menu_item_id ) ) {
+				wp_send_json_error( __( 'UM Warning: The menu_item_id is required', 'ultimate-member' ) );
+			}
 
 			foreach ( self::$fields as $_key => $label ) {
-
 				$key = sprintf( 'menu-item-%s', $_key );
 
 				// Sanitize
-				if ( !empty( $_POST[$key][$menu_item_db_id] ) ) {
-					// Do some checks here...
-					$value = is_array( $_POST[$key][$menu_item_db_id] ) ? array_keys( $_POST[$key][$menu_item_db_id] ) : $_POST[$key][$menu_item_db_id];
-				} else {
-					$value = null;
+				$value = null;
+				if ( isset( $_POST[$key] ) && isset( $_POST[$key][$menu_item_id] ) ) {
+					$value = is_array( $_POST[$key][$menu_item_id] ) ? array_keys( $_POST[$key][$menu_item_id] ) : $_POST[$key][$menu_item_id];
 				}
 
 				// Update
 				if ( !is_null( $value ) ) {
-					update_post_meta( $menu_item_db_id, $key, $value );
+					update_post_meta( $menu_item_id, $key, $value );
 				}
 			}
+
+			wp_send_json_success( sprintf( __( 'The menu item %1$s is updated', 'ultimate-member' ), $menu_item_id ) );
 		}
 
 		/**
@@ -206,10 +218,10 @@ if ( !class_exists( 'um\admin\core\Admin_Navmenu' ) ) {
 
 			if ( empty( self::$restriction_data[$item_id] ) ) {
 				$um_nav_public = get_post_meta( $item_id, 'menu-item-um_nav_public', true );
-				$_nav_roles_meta = (array) get_post_meta( $item_id, 'menu-item-um_nav_roles', true );
+				$um_nav_roles_meta = (array) get_post_meta( $item_id, 'menu-item-um_nav_roles', true );
 
 				$um_nav_roles = array();
-				foreach ( $_nav_roles_meta as $key => $value ) {
+				foreach ( $um_nav_roles_meta as $key => $value ) {
 					if ( is_int( $key ) ) {
 						$um_nav_roles[] = $value;
 					}
@@ -249,7 +261,7 @@ if ( !class_exists( 'um\admin\core\Admin_Navmenu' ) ) {
 				'um_nav_columns'   => 2
 					), $restriction_data ) );
 			?>
-			<div class="um-nav-edit">
+			<div class="um-nav-edit" data-menu_item_id="<?php echo esc_attr( $item_id ); ?>">
 				<div class="clear"></div>
 				<h4 style="margin-bottom: 0.6em;"><?php _e( "Ultimate Member Menu Settings", 'ultimate-member' ) ?></h4>
 
@@ -300,7 +312,8 @@ if ( !class_exists( 'um\admin\core\Admin_Navmenu' ) ) {
 		 */
 		public function wp_nav_menu_item_custom_fields_customize_template() {
 			?>
-			<div class="um-nav-edit">
+			<# if( typeof( data.restriction_data ) === 'object' ) { #>
+			<div class="um-nav-edit" data-menu_item_id="{{ data.menu_item_id }}">
 				<div class="clear"></div>
 				<h4 style="margin-bottom: 0.6em;"><?php _e( "Ultimate Member Menu Settings", 'ultimate-member' ) ?></h4>
 
@@ -324,6 +337,7 @@ if ( !class_exists( 'um\admin\core\Admin_Navmenu' ) ) {
 
 				</p>
 			</div>
+			<# } #>
 			<?php
 		}
 
